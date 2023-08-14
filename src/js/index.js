664fc3824a7f40a557023820ed2879e99d024a4d
Igor Bodrov <bodrovigor02@gmail.com>
2023-08-14 17:54:51 +0300

    feat: 🎸 Разметка, стилизация

--- src/js/index.js
+++ src/js/index.js
@@ -128,69 +128,98 @@ window.addEventListener('load', () => {
   })
 })
 
-// ? Загрузка файлов в блоке обратной связи
+
+// ? --- Загрузка файлов в блоке обратной связи
 let attach_input = document.getElementById('attach_input');
 let attach_button = document.getElementById('attach_button');
 let attach_list = document.getElementById('attach_list');
 
-let inputFiles = [];
+// ? - Функция проверки количества и размера файлов
+function checkLengthAndSize(arr) {
+  let array = (Array.from(arr));
 
-function addItem(event) {
-  if (event.target.files.length > 10) {
-    alert('Не более 10 изображений!');
-    attach_input.value = '';
-    attach_list.innerHTML = '';
-
-    console.log(inputFiles);
-    return;
-  }
-  else if (event.target.files.length + inputFiles.length > 10) {
-    alert('Не более 10 изображений!');
-
-    console.log(inputFiles);
+  if (array.length > 10) {
+    alert('Количество файлов не должно превышать 10');
     return;
   }
   else {
-    !inputFiles.length ? inputFiles = Array.from(event.target.files) : inputFiles = inputFiles.concat(Array.from(event.target.files));
-    attach_list.innerHTML = '';
+    let resultArray = [];
 
-    console.log(inputFiles);
-
-    inputFiles.forEach(file => {
-      if ((file.size / (1024 * 1024)).toFixed(2) > 20) {
-        inputFiles.splice(inputFiles.indexOf(file), 1);
-        alert(`Файл ${file.name} слишком большой`);
-      }
-      else {
-        const reader = new FileReader();
-
-        reader.onload = ev => {
-          const src = ev.target.result;
-          attach_list.insertAdjacentHTML('beforeend', `
-  
-            <li class="feedback__form-attach-item">
-              <button class="feedback__form-attach-cancel base-button" type="button" data-name="${file.name}"></button>
-              <a class="base-link" href="${src}" data-fancybox>
-                <img class="feedback__form-attach-img" src="${src}" width="40" height="40" alt="${file.name}">
-              <a>
-            </li>
-            
-          `);
-        }
-        reader.readAsDataURL(file);
-      }
+    array.map(item => {
+      ((item.size / (1024 * 1024)).toFixed(2)) > 20 ?
+        alert(`Файл ${item.name} слишком большой`) :
+        resultArray.push(item);
     })
+
+    return resultArray;
   }
 }
 
+// ? - Функция для отрисовки результирующего массива
+function renderList(arr, target) {
+  let array = Array.from(arr);
+
+  attach_list.innerHTML = '';
+
+  array.forEach(item => {
+    const reader = new FileReader();
+
+    reader.onload = ev => {
+      const src = ev.target.result;
+      target.insertAdjacentHTML('beforeend', `
+
+        <li class="feedback__form-attach-item">
+          <button class="feedback__form-attach-cancel base-button" type="button" data-name="${item.name}"></button>
+          <a class="base-link" href="${src}" data-fancybox>
+            <img class="feedback__form-attach-img" src="${src}" width="40" height="40" alt="${item.name}">
+          </a>
+        </li>
+        
+      `);
+    }
+    reader.readAsDataURL(item);
+  })
+}
+
+let inputFiles = [];
+
+// ? - Функция для добавления элемента
+function addItem(event) {
+
+  if (inputFiles.length === 0) {
+    (function () {
+      if (!checkLengthAndSize(event.target.files)) {
+        return;
+      }
+      else {
+        inputFiles = checkLengthAndSize(event.target.files);
+        renderList(inputFiles, attach_list);
+      }
+    }());
+  }
+
+  else {
+    (function () {
+      if (!checkLengthAndSize(event.target.files) || inputFiles.length + checkLengthAndSize(event.target.files).length > 10) {
+        alert('Количество файлов не должно превышать 10');
+        return;
+      }
+      else {
+        inputFiles = inputFiles.concat(checkLengthAndSize(event.target.files));
+        renderList(inputFiles, attach_list);
+      }
+    }());
+  }
+
+}
+
+// ? - Функция для удаления элемента
 function removeItem(event) {
   if (!event.target.dataset.name) {
     return;
   }
   let name = event.target.dataset.name;
   inputFiles = inputFiles.filter(file => file.name !== name);
-
-  console.log(inputFiles);
 
   let block = attach_list.querySelector(`[data-name="${name}"]`).closest('.feedback__form-attach-item');
   block.remove();
@@ -199,6 +228,5 @@ function removeItem(event) {
 attach_button.addEventListener('click', () => {
   attach_input.click();
 })
-
 attach_input.addEventListener('change', addItem);
 attach_list.addEventListener('click', removeItem);
